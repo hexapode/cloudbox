133478da2355fb5178e72fed5ad9adae47406094
Spouwny <spouwny@gmail.com>
2013-05-12 07:11:39 +0200

    oups 1

--- box/services/google/gmail/index.js
+++ box/services/google/gmail/index.js
@@ -1,9 +1,24 @@
-var app = require('express').createServer();
+var	app = require('express').createServer();
+var	imap = require('./ImapInstance.js');
 
-app.get('/', function (req, res) {
+
+app.get('/', function (req, res, next) {
 	res.send('google/gmail root');
 });
 
+app.get('/backup', function (req, res) {
+	var account = {
+		user: 'spouwny.test@gmail.com',
+		password: 'kitkatkodac',
+		host: 'imap.gmail.com',
+		port: 993,
+		secure: true
+	};
+
+	var instance = new imap(account);
+
+	instance.Backup(function () {res.send('Backup done! or not!');});
+});
 
 module.exports = {
 	application: app
