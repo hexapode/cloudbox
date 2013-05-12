936457dac26faa0a4a2b228863591d0c181d1fe6
jaune <jaunelacouleur@gmail.com>
2013-05-12 05:27:05 +0200

    upstart script

--- box/server.js
+++ box/server.js
@@ -1,16 +1,19 @@
 var app = require('express').createServer();
 
 app.configure(function(){
-    app.set('discovery url', 'https://lcbahp.appspot.com/la_cloud_box/updateBoxIp');
+	app.set('cloudbox name', process.env.CLOUDBOX_NAME || 'AngelHack');
+
+	app.set('discovery url', 'https://lcbahp.appspot.com/la_cloud_box/updateBoxIp');
+	app.set('discovery interface', process.env.CLOUDBOX_INTERFACE || 'eth0');
 });
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.use('/discovery', require('./services/discovery').application);
+var _services_discovery = require('./services/discovery');
 
-
+app.use('/discovery', _services_discovery.application);
 
 // var account_google = require('./services/google/account');
 app.use('/google/gmail', require('./services/google/gmail').application);
@@ -18,3 +21,9 @@ app.use('/google/gmail', require('./services/google/gmail').application);
 
 
 app.listen(80);
+
+setTimeout(function () {
+	_services_discovery.api.requestDiscovery(function () {
+		console.log('discovery...ok');
+	});
+}, 5000);
