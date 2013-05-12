3755b8001123b82f9072c2f8ae00b8609c1ce375
Guillaume Tournier <guillaume.tournier+github@gmail.com>
2013-05-12 05:17:59 +0200

    le fake pour la demo

--- box/services/google/gmail/index.js
+++ box/services/google/gmail/index.js
@@ -4,6 +4,10 @@ app.get('/', function (req, res) {
 	res.send('google/gmail root');
 });
 
+app.get('/create', function (req, res) {
+	res.render('google-gmail-create');
+});
+
 
 module.exports = {
 	application: app
